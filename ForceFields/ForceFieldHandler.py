362a574a8a6ea0c1526188e48563f149de5fb226
davidcooper7 <exouser@willingly-wallowing-wasp.js2local>
2024-03-20 23:17:51 +0000

    Update ForceField Documentation

--- ForceFields/ForceFieldHandler.py
+++ ForceFields/ForceFieldHandler.py
@@ -16,23 +16,51 @@ from openmm.unit import *
 
 class ForceFieldHandler():
     """
-    A Class for parameterization of a structure file.  Returns the necessary elements to 
-    construct an openmm simulation, System, Topology, and Positions.
-        SDF File - Ligand to be parameterized with OpenFF
-        PDB File - Environment File to be parameterized with Amber FF14SB, Lipid17, OPC3
-        XML Files - Override the defaults with optional user specified files.
-                    Extensions must be offxml for an sdf file or xml for a pdb file.
-        
-    Default Usage
+    A Class for parameterization of a structure file. Returns the necessary elements to construct an openmm simulation, System, Topology, and Positions. SDF File - Ligand to be parameterized with OpenFF PDB File - Environment File to be parameterized with Amber FF14SB, Lipid17, OPC3 XML Files - Override the defaults with optional user specified files. Extensions must be offxml for an sdf file or xml for a pdb file.
+    
+    Default Usage:
+    --------------
         system, topology, positions = ForceFieldHandler(INPUTS).main()
     
     Parameters:
+    -----------
         structure_file - Input, either and an sdf or pdb file
-        force_field_files - Optional Input - offxml or xml files.  Can use included forcefields with either OpenFF or OpenMM
-
+        force_field_files - Optional Input - offxml or xml files. Can use included forcefields with either OpenFF or OpenMM
+    
     Returns:
+    -------
         system, topology, positions as a 3-tuple
+    
+    Attributes:
+    -----------
+        default_xmls (dict): 
+            Default XML files for OpenFF and OpenMM.
+            
+        structure_file (str): 
+            Input file path, either an SDF or PDB file.
+            
+        working_mode (str): 
+            Mode of operation based on file extension, either 'OpenFF' or 'OpenMM'.
+            
+        xmls (list): 
+            List of XML files to be used for parameterization.
+    
+    Methods:
+    --------
+        __init__(self, structure_file, force_field_files=None, use_defaults: bool=True): 
+            Initializes the ForceFieldHandler object with a structure file and optional force field files.
+            
+        _parse_file(self, file_fn): 
+            Determines the working mode ('OpenFF' or 'OpenMM') based on the file extension.
         
+        main(self, use_rdkit: bool=False): 
+            Main method for parameterization. Returns a tuple containing the system, topology, and positions.
+        
+        generate_custom_xml(self, out_xml, name): 
+            Generates a custom XML file for parameterization.
+        
+        neutralizeMol(mol): 
+            Neutralizes the given molecule by setting radical electrons and formal charges to zero.
     """
     
     def __init__(self, structure_file, force_field_files=None, use_defaults: bool=True):
@@ -109,9 +137,14 @@ class ForceFieldHandler():
 
         return (sys, top, positions)
 
-    def generate_custom_xml(self, out_xml, name):
+    def generate_custom_xml(self, out_xml: str, name: str):
         """
-        Generate a custom .xml to pass to Handler
+        Generate a custom .xml to pass to Handler.
+
+        Parameters:
+        -----------
+            out_xml (str): 
+                String path to output.xml file. 
         """
 
         # Invoke main to create interchange object
@@ -137,12 +170,10 @@ class ForceFieldHandler():
         # Use write_xml_pretty.py to convert .prmtop to .xml
         os.system(f'python {pathlib.Path(__file__).parent.resolve()}/write_xml_pretty.py -i {input_json}')  
 
-        return out_xml
-
-def neutralizeMol(mol):
-    for a in mol.GetAtoms():
-        if a.GetNumRadicalElectrons()==1:
-             a.SetNumRadicalElectrons(0)         
-        if a.GetFormalCharge()!=0:
-             a.SetFormalCharge(0)         
-    return mol
+# def neutralizeMol(mol):
+#     for a in mol.GetAtoms():
+#         if a.GetNumRadicalElectrons()==1:
+#              a.SetNumRadicalElectrons(0)         
+#         if a.GetFormalCharge()!=0:
+#              a.SetFormalCharge(0)         
+#     return mol
