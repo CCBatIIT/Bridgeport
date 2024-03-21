a5926fd35398acc867f36ee6454c716aeec8cfbc
josephdepaoloboisvert <jdepaoloboisvert@hawk.iit.edu>
2024-03-21 16:38:26 +0000

    Update Joiner Documentation

--- ForceFields/OpenMMJoiner.py
+++ ForceFields/OpenMMJoiner.py
@@ -12,7 +12,9 @@ from openmm.unit import *
 
 class Joiner():
     """
-    A Class for adding two OpenMM system, topology, and position sets. This tool is intended for use cases such as adding an OpenFF Ligand System to an OpenMM Environment setup, allowing for comprehensive simulations involving both small molecules and complex biological systems.
+    A Class for adding two OpenMM system, topology, and position sets.
+    This tool is intended for use cases such as adding an OpenFF Ligand System to an OpenMM Environment setup,
+    allowing for comprehensive simulations involving both small molecules and complex biological systems.
     
     Parameters:
     -----------
@@ -34,16 +36,27 @@ class Joiner():
             Initializes the Joiner instance with the provided Ligand and Receptor sets.
             
         main(self): 
-            Combines the ligand and receptor sets into a single set of system, topology, and positions. Returns a 3-tuple of these combined components.
+            Combines the ligand and receptor sets into a single set of system, topology, and positions.
+            Returns a 3-tuple of these combined components.
        
         _join_two_topologies(self, tops: tuple, poss: tuple): 
-            Joins two OpenMM topologies and their associated positions. The first index is added to the second. Parameters include a two-tuple of topologies and a two-tuple of positions.
+            Joins two OpenMM topologies and their associated positions. The first index is added to the second.
+            Parameters include a two-tuple of topologies and a two-tuple of positions.
             
         _join_two_systems(self, sys1: System, sys2: System): 
-            Joins two OpenMM systems by adding the elements of system1 to system2. This method is intended to combine a ligand system with a receptor system. Parameters include sys1 (the system to be added) and sys2 (the system to which sys1 is added).
+            Joins two OpenMM systems by adding the elements of system1 to system2.
+            This method is intended to combine a ligand system with a receptor system.
+            Parameters include sys1 (the system to be added) and sys2 (the system to which sys1 is added).
     """
     
     def __init__(self, Ligand_Set, Receptor_Set):
+        """
+        Define the two systems to be added to each other (Ligand_Set onto Receptor_Set)
+
+        Parameters:
+            Ligand_Set: tuple: A 3-tuple of OpenMM System, OpenMM Topology, and Positions array
+            Receptor_Set: tuple: A 3-tuple of OpenMM System, OpenMM Topology, and Positions array
+        """
         assert type(Ligand_Set) == type(Receptor_Set) and len(Ligand_Set) == len(Receptor_Set)
         assert False not in [type(Ligand_Set[i]) == type(Receptor_Set[i]) for i in range(3)]
 
@@ -53,8 +66,13 @@ class Joiner():
 
     def main(self):
         """
-        Returns
-            Joined_Set: (System, Topology, Positions) as 3-tuple
+        Main usage case - wrapping together other methods to perform the addition of OpenMM objects (System, Topology, Positions)
+
+        Parameters:
+            None
+            
+        Returns:
+            Joined_Set: tuple: A 3-tuple of the joined System, Topology, and Positions array
         """
         comp_top, comp_positions = self._join_two_topologies((self.rec_top, self.lig_top), (self.rec_pos, self.lig_pos))
         comp_sys = self._join_two_systems(self.lig_sys, self.rec_sys)
@@ -62,13 +80,14 @@ class Joiner():
     
     def _join_two_topologies(self, tops: tuple, poss: tuple):
         """
-        Joins two topologies by adding the second to the first
-        INDEX 1 IS ADDED TO INDEX 0
+        Joins two topologies and positions arrays.  Tops (2-tuple of Topologies) and Poss (2-tuple of positions arrays) are
+        joined together by adding index 1 onto index 0 respectively.
+        
         Parameters:
-            tops: A two-tuple of openmm Topologies
-            poss: A two-tuple of positions
+            tops: tuple: A two-tuple of OpenMM Topologies
+            poss: tuple: A two-tuple of Position Arrays
         Returns
-            2-Tuple: Topology, positions of the joined atoms
+            topology, positions: tuple: A 2-tuple of the topology and position array which are joined
         """
         assert len(tops) == 2 and len(poss) == 2
         modeller = Modeller(tops[0], poss[0])
@@ -77,14 +96,21 @@ class Joiner():
 
     def _join_two_systems(self, sys1: System, sys2: System):
         """
-        Joins Two Openmm systems by adding the elements of system 1 to system 2
-        Intended use in this notebook is join_two_systems(ligand_sys, receptor_sys)
+        Joins Two Openmm systems by adding the each Particle, Constraint, PeriodicTorsionForce, HarmonicAngleForce, HarmonicBondForce, and NonBondedForce
+        from OpenMM System 1 (sys1) to OpenMM System 2 (sys2)
+
+        Intended use is _join_two_systems(LIGAND_SYS, RECEPTOR_SYS)
         
+        Due to slight discrepencies in the way that OpenFF and OpenMM name forces, this function is hardcoded to add sys1 to sys2, where:
+            sys1 is an OpenMM system that was created by an OpenFF interchange
+            sys2 is an OpenMM system that was created by OpenMM
+                
         Parameters:
-            sys1 - The openmm system to be added to sys2
-            sys2 - The openmm system to have sys1 added to
-        Returns
-            System - The combined system
+            sys1: System: An OpenMM system that was created by an OpenFF interchange.  The OpenMM System TO BE ADDED to sys2
+            sys2: System: An OpenMM system that was created by OpenMM.  The OpenMM System to have sys1 ADDED TO
+        
+        Returns:
+            sys2: System: The input OpenMM System (sys2) with the addition of all parameters from sys1
         """
         #Particles
         new_particle_indices = []
