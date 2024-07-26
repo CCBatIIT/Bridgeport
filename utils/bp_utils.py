5e9be3299eaaa6dc6034fff8d381a149e7f39881
davidcooper7 <117563200+davidcooper7@users.noreply.github.com>
2024-07-26 15:23:32 -0500

    Update bp_utils.py

--- utils/bp_utils.py
+++ utils/bp_utils.py
@@ -409,7 +409,8 @@ def match_internal_coordinates(ref_match: mda.AtomGroup, ref_match_atoms: List, 
             try:
                 c1, c2, c3, c4 = ref_tors_sele.positions
             except:
-                print(ref_tors_sele.atoms.names, ref_tors_sele.atoms.resids)
+                print('reference atoms names attempted to match': ref_tors_sele.atoms.names, 'reference resids attempted to match', ref_tors_sele.atoms.resids, flush=True)
+                raise Exception("Could not match torsion")
             dihedral = calc_dihedrals(c1, c2, c3, c4)
             mobile_tors[i] = dihedral
             #changed[mobile_R._primary_torsion_indices[i]] = True
